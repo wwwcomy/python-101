7f654f3d30c980d0848dcaeed2744d41fcf6157d
wwwcomy <wwwcomy@gmail.com>
2019-05-30 23:30:29 +0800

    show me the code 0000

---
+++ showMeTheCode/D1.py
@@ -0,0 +1,8 @@
+from PIL import Image, ImageDraw, ImageFont
+from PIL import PSDraw
+im = Image.open("pic.jpg")
+myfont = ImageFont.truetype('C:/windows/fonts/Arial.ttf', size=40)
+draw = ImageDraw.Draw(im)
+print(im.size[0]-40)
+draw.text((im.size[0]-40,0),"4",font=myfont,fill="#ff0000")
+im.save('result.jpg','jpeg')
