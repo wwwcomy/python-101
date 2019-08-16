0b1b6d1d7a1715c2c605b632f1a949f4b17224a7
xingnliu <wwwcomy@gmail.com>
2019-08-16 11:08:09 +0800

    use openpyxl to process excel

--- showMeTheCode/D14-ExcelWriter.py
+++ showMeTheCode/D14-ExcelWriter.py
@@ -1,1 +1,28 @@
-# TODO use URL lib and re
+from openpyxl import Workbook
+dict = {}
+try:
+    with open("student.txt","r") as student:
+        content = student.read()
+        dict = eval(content)
+        print(content)
+        print(dict)
+except IOError as err:
+    print(str(err))
+
+print(dict['1'])
+
+wb = Workbook()
+
+# grab the active worksheet
+ws = wb.active
+
+arr = []
+for key in dict:
+    arr.clear()
+    arr.append(key)
+    arr.extend(dict.get(key))
+    # Rows can also be appended
+    ws.append(arr)
+
+# Save the file
+wb.save("student.xlsx")
