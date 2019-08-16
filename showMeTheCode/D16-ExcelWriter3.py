867f35f180a105e4d8f8d84f03399f154fb07ddc
xingnliu <wwwcomy@gmail.com>
2019-08-16 11:22:15 +0800

    add excel writer D16.

--- showMeTheCode/D16-ExcelWriter3.py
+++ showMeTheCode/D16-ExcelWriter3.py
@@ -1,1 +1,23 @@
-# TODO use URL lib and re
+from openpyxl import Workbook
+dict = []
+try:
+    with open("numbers.txt","r") as numbers:
+        content = numbers.read()
+        dict = eval(content)
+except IOError as err:
+    print(str(err))
+
+print(dict)
+print(dict[0])
+print(dict[0][0])
+
+wb = Workbook()
+
+# grab the active worksheet
+ws = wb.active
+
+for row in dict:
+    ws.append(row)
+
+# Save the file
+wb.save("D16-numbers.xlsx")
